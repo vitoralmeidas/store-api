6a0c9843a9bd24006054ba049d9e33ad5cd8c16a
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-07-05 11:46:41 -0300

    testing the routes + controllers

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 const app = express();
 
 const connectDB = require("./db/connect");
+const router = require("./routes/products");
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
   res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
 });
 
-app.use("/api/v1/products");
+app.use("/api/v1/products", router);
 
 //products route
 app.use(notFoundMiddleware);
