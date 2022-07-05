b691869b585614f571edef2941c614093ecb1c8e
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-07-05 11:36:13 -0300

    server connected to db

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -15,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("<h1>Store API</h1><a href='/api/v1/products'>products route</a>");
 });
 
+app.use("/api/v1/products");
+
 //products route
 app.use(notFoundMiddleware);
 app.use(errorMiddleware);
@@ -22,7 +26,7 @@ app.use(errorMiddleware);
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
-    // connect DB
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => console.log(`Listening on Port ${port}`));
   } catch (error) {
     console.log(error);
