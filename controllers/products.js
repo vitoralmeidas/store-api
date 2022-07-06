9d6a189de29b02fdc4f22b52007fe4c4016ae8e4
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-07-06 11:26:24 -0300

    contrllers -> filters

--- controllers/products.js
+++ controllers/products.js
@@ -7,10 +7,17 @@ const getAllProductsStatic = async (req, res) => {
 
 const getAllProducts = async (req, res) => {
   // query params => http://.../products?<featured=price=23>
-  const { featured } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = name;
   }
 
   console.log(queryObject);
