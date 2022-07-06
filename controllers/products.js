807b30c431c473911b235dd57e13d014c872f3f6
vitoralmeidas <vitor.oliveira1904@gmail.com>
2022-07-06 16:35:09 -0300

    numericFilters... query

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,9 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({ price: { $gt: 30 } })
+    .sort("price")
+    .select("price");
   res.status(200).json({ products, nbHits: products.length });
 };
 
@@ -39,6 +41,13 @@ const getAllProducts = async (req, res) => {
     result = result.select(fieldList);
   }
 
+  //limit
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+  // 23
   // console.log(queryObject);
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
